30338bb5c35cc1d16a288a162f5fb2db4f536c9b
Ilya Verbitskiy <iverbitskiy@gmail.com>
2017-06-08 20:27:45 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const {app, BrowserWindow, Menu} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow () {
+    win = new BrowserWindow({
+        show: false
+    });
+
+    Menu.setApplicationMenu(null);
+    win.maximize();
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'app/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.once('ready-to-show', () => {
+        win.show();
+    });
+
+    win.on('closed', () => {
+        win = null
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+});
